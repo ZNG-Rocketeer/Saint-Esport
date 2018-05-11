7ab8d06b419a74b18fd8ac06a69e12efab874e9f
Mickael <mickael.karche@etu.univ-st-etienne.fr>
2018-05-11 17:41:30 +0200

    d

--- forum/pdo_new_topics.php
+++ forum/pdo_new_topics.php
@@ -2,10 +2,10 @@
 echo $_SESSION['mail'];
 if (isset($_POST['sujet']) && isset($_POST['post'])) {
   try{
-    $new = $pdo->prepare("INSERT INTO Forum_sujet(sujet, mail) VALUES (?,?)");
-    $new->bindParam(1,$_POST['sujet']);
-    $new->bindParam(2,$_SESSION['mail']);
-    $new->execute();
+    $new_topics = $pdo->prepare("INSERT INTO Forum_sujet(sujet, mail) VALUES (?,?)");
+    $new_topics->bindParam(1,$_POST['sujet']);
+    $new_topics->bindParam(2,$_SESSION['mail']);
+    $new_topics->execute();
   }
   catch (Exception $e){
     die('Erreur : ' . $e->getMessage());
