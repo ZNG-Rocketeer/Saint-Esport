92aef9fc586efc9a57ee042672ecde9608d2df21
Mickael <mickael.karche@etu.univ-st-etienne.fr>
2018-05-12 21:16:37 +0200

    g

--- index.php
+++ index.php
@@ -21,7 +21,12 @@
     <p> Fonction pour tester apm </p>
   </aside>
   <article>
-    Article1<br/>Blalbalbalablalbablalb<br/>BLBlblabla
+    <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+      <input type="hidden" name="cmd" value="_s-xclick">
+      <input type="hidden" name="hosted_button_id" value="LDQVHRAWACSQU">
+      <input type="image" src="https://www.paypalobjects.com/fr_FR/FR/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal, le réflexe sécurité pour payer en ligne">
+      <img alt="" border="0" src="https://www.paypalobjects.com/fr_FR/i/scr/pixel.gif" width="1" height="1">
+    </form>    
   </article>
   <article>
     Article2<br/>fojiaefnpeifnaihdfzenbdpze<br/>
