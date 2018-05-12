bb5fa2d51711f341c4cc8b0be9c5a9b802a6a5cc
Tisamak <tanouvil@hotmail.fr>
2018-05-13 00:14:52 +0200

    d

--- Forum/liste_sujet.php
+++ Forum/liste_sujet.php
@@ -12,23 +12,23 @@ try{
     $result_list_user=$liste_user->fetch();
     if($i % 2 == 0)
     {
-      echo '
+      echo "
       <tr class="pair">
       <td><a href="discussion_forum.php?idsuj='.$result_list[$i]["id_sujet"].'">'.$result_list[$i]["sujet"].'</a></td>
       <td>'.$result_list_user['pseudo'].'</td>
       <td>'.$result_list[$i]["date"].'</td>
       </tr>
-      ';
+      ";
     }
     else
     {
-      echo '
+      echo "
       <tr class="impair">
       <td><a href="discussion_forum.php?idsuj='.$result_list[$i]["id_sujet"].'">'.$result_list[$i]["sujet"].'</a></td>
       <td>'.$result_list_user['pseudo'].'</td>
       <td>'.$result_list[$i]["date"].'</td>
       </tr>
-      ';
+      ";
     }
   }
 }
