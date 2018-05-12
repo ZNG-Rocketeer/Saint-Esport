744e7681d58e8b0b6888ce1070d8ced986668de5
Tisamak <tanouvil@hotmail.fr>
2018-05-13 00:20:25 +0200

    d

--- Forum/liste_sujet.php
+++ Forum/liste_sujet.php
@@ -14,7 +14,7 @@ try{
     {
       echo '
       <tr style="background-color:#FEFEFE;">
-      <td><a href="discussion_forum.php?idsuj='.$result_list[$i]["id_sujet"].'">'.$result_list[$i]["sujet"].'</a></td>
+      <td><a class="sujet" href="discussion_forum.php?idsuj='.$result_list[$i]["id_sujet"].'">'.$result_list[$i]["sujet"].'</a></td>
       <td>'.$result_list_user['pseudo'].'</td>
       <td>'.$result_list[$i]["date"].'</td>
       </tr>
