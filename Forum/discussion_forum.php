1999248916f4b4a3e4eea022cbb1e95769df6ad9
Mickael <mickael.karche@etu.univ-st-etienne.fr>
2018-05-12 18:08:03 +0200

    f

--- Forum/discussion_forum.php
+++ Forum/discussion_forum.php
@@ -16,16 +16,20 @@
     include '../pdo_init.php';
     try{
       if(isset($_GET['idsuj'])){
+        $liste_user=$pdo->prepare("SELECT pseudo FROM zng_user WHERE mail= ?");
         $idsuj=$_GET['idsuj'];
         $discuss=$pdo->prepare("SELECT * FROM Forum_posts WHERE sujet=?");
         $discuss->bindParam(1,$idsuj);
         $discuss->execute();
         $result_discuss=$discuss->fetchAll();
         for ($i=0; $i < $discuss->rowCount(); $i++) {
+          $liste_user->bindParam(1,$result_discuss[$i]["mail"]);
+          $liste_user->execute();
+          $result_list_user=$liste_user->fetch();
           echo '
           <tr>
           <td>'.$result_discuss[$i]["contenu"].'</a></td>
-          <td>'.$result_discuss[$i]["mail"].'</td>
+          <td>'.$result_list_user['pseudo'].'</td>
           <td>'.$result_discuss[$i]["date"].'</td>
           </tr>
           ';
