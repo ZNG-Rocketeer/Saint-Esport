be874727bf1692b6e61ca1045018ed489a417808
Tisamak <tanouvil@hotmail.fr>
2018-05-13 00:41:57 +0200

    d

--- Forum/new_discussion.php
+++ Forum/new_discussion.php
@@ -17,7 +17,7 @@
     </label>
     <br/>
     <label>
-      <textarea rows="4" cols="50" name="post" placeholder="Contenu du post" required="required"></textarea>
+      <textarea rows="4" cols="50"name="post" placeholder="Contenu du post" required="required"></textarea>
     </label>
 
     <?php
