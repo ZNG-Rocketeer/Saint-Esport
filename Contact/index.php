9bfa2f9a6bddfadaafc46cc72840ad76b48abb5f
Tisamak <tanouvil@hotmail.fr>
2018-05-12 23:33:01 +0200

    a

---
+++ Contact/index.php
@@ -0,0 +1,27 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="../master.css">
+  <title> Sainté E-sport </title>
+</head>
+
+<body>
+  <!-- HEADER -->
+  <?php include("../Include/entete.php"); ?>
+
+  <h1 > Contact </h1>
+
+  <p>
+    Contact 1, Contact 2, Contact 3.
+  </p>
+  <footer>
+  </footer>
+</body>
+
+
+
+
+
+</html>
---
+++ Partenaires/index.php
@@ -0,0 +1,25 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="../master.css">
+  <title> Sainté E-sport </title>
+</head>
+
+<body>
+  <!-- HEADER -->
+  <?php include("../Include/entete.php"); ?>
+
+  <h1 > Partenaires </h1>
+
+  <p>
+    Voici les différents partenaires de la lyon esport !
+
+  </p>
+
+  <footer>
+  </footer>
+</body>
+
+</html>
