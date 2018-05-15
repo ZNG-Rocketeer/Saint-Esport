5049e975a3e669cefc9d5a9854270f8ffa35b710
Tisamak <tanouvil@hotmail.fr>
2018-05-15 19:12:54 +0200

    dz

--- Include/footer.php
+++ Include/footer.php
@@ -1,9 +1,13 @@
 <?php
 echo '
 <footer>
-<div class="centrer">
+<table id="footer">
+<tr>
+<td>
 Sainté Esport est un site fait par des fans pour des fans en collaboration avec Zanga Esport. Tous droits réservés.
-</div>
+</td>
+</tr>
+</table>
 </footer>
 ';
 ?>
