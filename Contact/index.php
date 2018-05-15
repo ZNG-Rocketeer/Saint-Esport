d7f7098de86795ea54dca968f11de92b9b4f81d3
Tisamak <tanouvil@hotmail.fr>
2018-05-15 20:04:57 +0200

    Clear.

--- Contact/index.php
+++ Contact/index.php
@@ -15,7 +15,8 @@
     <h2>Contact</h2>
     <p>
       Si vous avez des questions concernant l'institution Zanga ou des questions en rapport avec le déroulement de la lan,
-      <br/>visitez d'abord cette partie du site et si vous ne trouvez pas réponse, envoyez un email à : Zanga@Esport.fr.
+      <br/>visitez d'abord cette partie du site et si vous ne trouvez pas réponse, envoyez un email à : <span class=mail_contact>
+       Zanga@Esport.fr</span>.
       <br/>
       Nous nous efforcerons de répondre dans les plus bref délais.
     </p>
