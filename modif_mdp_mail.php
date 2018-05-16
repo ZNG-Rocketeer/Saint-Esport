386e22f61d64b500d3b328f75ab32d86009d3bed
Mickael <mickael.karche@etu.univ-st-etienne.fr>
2018-05-16 19:57:22 +0200

    blabla

--- modif_mdp_mail.php
+++ modif_mdp_mail.php
@@ -4,7 +4,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 //Load Composer's autoloader
-require '../vendor/autoload.php';
+require 'vendor/autoload.php';
 
 $mail = new PHPMailer(true);                              // Passing `true` enables exceptions
 try {
