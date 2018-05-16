7355fcd23585d3bda2d2630e85aa54a2ab0690f5
Tisamak <tanouvil@hotmail.fr>
2018-05-16 20:36:56 +0200

    Auto stash bedzafore merge of "master" and "origin/master"
    dza

--- Infos/inscription.php
+++ Infos/inscription.php
@@ -37,7 +37,7 @@
         <input type="hidden" name="cmd" value="_s-xclick">
         <input type="hidden" name="hosted_button_id" value="LDQVHRAWACSQU">
         <input type="image" src="https://www.paypalobjects.com/fr_FR/FR/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal, le réflexe sécurité pour payer en ligne">
-        <img alt="" border="0" src="https://www.paypalobjects.com/fr_FR/i/scr/pixel.gif" width="1" height="1">
+        <img alt="" src="https://www.paypalobjects.com/fr_FR/i/scr/pixel.gif" width="1" height="1">
       </form>
     </div>';
     }
