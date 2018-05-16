e77698f8649c84b113dd40d08bb21dfab604d8f7
Mickael <mickael.karche@etu.univ-st-etienne.fr>
2018-05-16 20:39:13 +0200

    jhgfd

--- assets/footer.php
+++ assets/footer.php
@@ -4,7 +4,7 @@ echo '
 <table id="footer">
 <tr>
 <td>
-Zanga Esport est un site fait par des fans pour des fans en collaboration avec Zanga Esport. Tous droits réservés.
+ZNG-Esport est un site fait par des fans pour des fans en collaboration avec Zanga Esport. Tous droits réservés.
 </td>
 </tr>
 </table>
