5d5f782e25309511e60082cc31f02f7d16189efd
Tisamak <tanouvil@hotmail.fr>
2018-05-16 20:38:21 +0200

    dza

--- Infos/inscription.php
+++ Infos/inscription.php
@@ -36,7 +36,7 @@
       <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
         <input type="hidden" name="cmd" value="_s-xclick">
         <input type="hidden" name="hosted_button_id" value="LDQVHRAWACSQU">
-        <input type="image" src="https://www.paypalobjects.com/fr_FR/FR/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal, le réflexe sécurité pour payer en ligne">
+        <input type="image" src="https://www.paypalobjects.com/fr_FR/FR/i/btn/btn_buynowCC_LG.gif" name="submit" alt="PayPal, le réflexe sécurité pour payer en ligne">
         <img alt="" src="https://www.paypalobjects.com/fr_FR/i/scr/pixel.gif" width="1" height="1">
       </form>
     </div>';
